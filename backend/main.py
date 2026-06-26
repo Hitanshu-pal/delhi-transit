f86721f51b82b833aa2318bff918d3a30c480372
Hitanshu-pal <hitanshupal1727@gmail.com>
2026-06-27 00:33:36 +0530

    Complete backend - route and stop endpoints

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,15 @@ from graph.graph_cache import load_graph
 from contextlib import asynccontextmanager
 from pydantic import BaseModel
 from fastapi import Request
+from thefuzz import process
 from routing.dijkstra import dijkstra
 from routing.bfs import fewest_transfers
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     app.state.graph = load_graph()
+    G = app.state.graph
+    app.state.stop_index = {G.nodes[n]["name"]: n for n in app.state.graph.nodes}
     yield
 
 app = FastAPI(lifespan=lifespan)
@@ -54,3 +57,18 @@ def get_route(request: Request, body: RouteRequest):
             ]
         }
 
+@app.get("/stops")
+def search_stops(request: Request, q: str):
+    G = request.app.state.graph
+    index = request.app.state.stop_index
+    matches= process.extract(q, index.keys(), limit = 5)
+    return [
+            {
+                "id": index[name],
+                "name": name,
+                "mode": G.nodes[index[name]]["mode"],
+                "lat":  G.nodes[index[name]]["lat"],
+                "lon":  G.nodes[index[name]]["lon"]
+            }
+            for name, score in matches
+    ]
